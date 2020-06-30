b2205cabe5d424b03a1723a5a5da7a5c8462be1a
Chloe Steffens <caforonda103@yahoo.com>
2020-06-30 05:54:29 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,8 @@ class Users < ActiveRecord::Migration [5.2]
     create_table :users do |t|
       t.string :username
       t.string :password
-      t.string :balance
-    
+      t.float :balance
+    end
   end
-  
 end
 
